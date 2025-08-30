b939287fe8b570395da1ba39b476022fb1141e07
PorterZhang2021 <porterzhang2021@outlook.com>
2025-08-30 14:42:43 +0800

    feat: 实现DefaultServiceMessageSource
    Signed-off-by: PorterZhang2021 <porterzhang2021@outlook.com>

---
+++ mini-i18n-core/src/main/java/io/mini/i18n/DefaultServiceMessageSource.java
@@ -0,0 +1,36 @@
+package io.mini.i18n;
+
+import io.microsphere.text.FormatUtils;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DefaultServiceMessageSource extends PropertiesResourceServiceMessageSource{
+
+    protected static final String RESOURCE_LOCATION_PATTERN = "META-INF/i18n/{}/{}";
+
+    public DefaultServiceMessageSource(String source) {
+        super(source);
+    }
+
+    @Override
+    protected String getResource(String resourceName) {
+        return FormatUtils.format(RESOURCE_LOCATION_PATTERN, source, resourceName);
+    }
+
+    @Override
+    protected List<Reader> loadAllPropertiesResources(String resource) throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        Enumeration<URL> resources = classLoader.getResources(resource);
+        LinkedList<Reader> propertiesResources = new LinkedList<>();
+        while (resources.hasMoreElements()) {
+            URL url = resources.nextElement();
+            propertiesResources.add(new java.io.InputStreamReader(url.openStream(), getEncoding()));
+        }
+        return propertiesResources;
+    }
+}
---
+++ mini-i18n-study-note/src/main/java/io/mini/i18n/core/_11_Feat_Class_DefaultServiceMessageSource.java
@@ -0,0 +1,22 @@
+package io.mini.i18n.core;
+
+/**
+ * <p>
+ *     {@link io.mini.i18n.PropertiesResourceServiceMessageSource}实现完成后，我们首先实现{@link io.mini.i18n.DefaultPropertiesServiceMessageSource}这个类，
+ *     该类主要是一个默认实现，其主要需要完成如何获取Resource以及如何加载Resource也就是形成一个Reader，用来读取，而最终这里的读取其实都是存储到Properties都是交给了其
+ *     父类{@link io.mini.i18n.PropertiesResourceServiceMessageSource}来完成的, 其主要有两个方法需要实现
+ *     <ul>
+ *         <li>
+ *             {@link io.mini.i18n.DefaultPropertiesServiceMessageSource#getResource(String)} 该方法主要是用来获取对应的Resource资源的,
+ *             这里的实现主要是从META-INF/i18n/{}/{}下获取对应的资源文件。
+ *         </li>
+ *         <li>
+ *             {@link io.mini.i18n.DefaultPropertiesServiceMessageSource#loadAllPropertiesResources(String)} 该方法主要是用来加载对应的Resource资源的,
+ *             这里的实现主要是通过ClassLoader的getResources来获取对应的资源文件, 所以我们将获取到的resource传入后，我们就可以获取
+ *             到对应的资源文件列表，然后将其转换成Reader返回即可。
+ *         </li>
+ *     </ul>
+ * </p>
+ */
+public class _11_Feat_Class_DefaultServiceMessageSource {
+}
