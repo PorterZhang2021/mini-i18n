e038be24ead84414ad3fb073ceece3da147c8844
PorterZhang2021 <porterzhang2021@outlook.com>
2025-09-20 16:42:50 +0800

    feat: 构建ResourceServiceMessageSourceChangedEvent类用来获取对应的事件变化情况

--- mini-i18n-spring/src/main/java/io/mini/i18n/spring/context/ResourceServiceMessageSourceChangedEvent.java
+++ mini-i18n-spring/src/main/java/io/mini/i18n/spring/context/ResourceServiceMessageSourceChangedEvent.java
@@ -1,13 +1,19 @@
 package io.mini.i18n.spring.context;
 
-import org.springframework.context.ApplicationEvent;
 
-public class ResourceServiceMessageSourceChangedEvent extends ApplicationEvent {
-    public ResourceServiceMessageSourceChangedEvent(Object source) {
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.ApplicationContextEvent;
+
+public class ResourceServiceMessageSourceChangedEvent extends ApplicationContextEvent {
+
+    private final Iterable<String> changedResources;
+
+    public ResourceServiceMessageSourceChangedEvent(ApplicationContext source, Iterable<String> changedResources) {
         super(source);
+        this.changedResources = changedResources;
     }
 
     public Iterable<String> getChangedResources() {
-        return null;
+        return changedResources;
     }
 }
