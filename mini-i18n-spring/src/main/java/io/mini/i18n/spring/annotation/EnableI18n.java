e195100b2306053a7e2b7910ec0efd2ffb46c385
PorterZhang2021 <porterzhang2021@outlook.com>
2025-09-20 16:35:06 +0800

    feat: 构建EnableI18n注解，该注解用来开启国际化功能的注册

--- mini-i18n-spring/src/main/java/io/mini/i18n/spring/annotation/EnableI18n.java
+++ mini-i18n-spring/src/main/java/io/mini/i18n/spring/annotation/EnableI18n.java
@@ -1,4 +1,18 @@
 package io.mini.i18n.spring.annotation;
 
+import io.mini.i18n.ServiceMessageSource;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Documented
+@Inherited
+@Import(I18nImportBeanDefinitionRegister.class)
 public @interface EnableI18n {
+
+    String[] sources() default {ServiceMessageSource.COMMON_SOURCE};
+
+    boolean exposeMessageSource() default true;
 }
